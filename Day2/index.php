9a3ac2e3576b064107c955108e013e570ad5a1a5
Jatin Parab <sulphurgfx@gmail.com>
2018-12-25 12:03:59 +0530

    Add day2

---
+++ Day2/index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Bootcamp Day 2</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row number-row">
+            <div  class="col-sm-4 number-left">
+                <h1 class="text-center"> 1 </h1>
+            </div>
+            <div  class="col-sm-4 number-center">
+                <h1 class="text-center"> 2 </h1>
+            </div>
+            <div class="col-sm-4 number-right">
+                <h1 class="text-center"> 3 </h1>
+            </div>
+        </div>
+        <div  class="row number-row">
+            <div  class="col-sm-4 number-left">
+                <h1 class="text-center"> 4 </h1>
+            </div>
+            <div  class="col-sm-4 number-center">
+                <h1 id="center" class="text-center"> 5 </h1>
+            </div>
+            <div  class="col-sm-4 number-right">
+                <h1 class="text-center"> 6 </h1>
+            </div>
+        </div>
+        <div  class="row number-row">
+            <div  class="col-sm-4 number-left">
+                <h1 class="text-center"> 7 </h1>
+            </div>
+            <div class="col-sm-4 number-center">
+                <h1 class="text-center"> 8 </h1>
+            </div>
+            <div class="col-sm-4 number-right">
+                <h1 class="text-center"> 9 </h1>
+            </div>
+        </div>
+    </div>
+
+
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</body>
+
+</html>
